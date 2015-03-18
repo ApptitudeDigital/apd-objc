40c5781213cb83e042841eac443aac56fd40698f
Aaron Smith <gngrwzrd@gmail.com>
2015-03-17 18:34:41 -0700

    Updated UIAlertView additions

--- UIAlertView+Additions.h
+++ UIAlertView+Additions.h
@@ -3,6 +3,9 @@
 
 @interface UIAlertView (Additions)
 
++ (UIAlertView *) alertWithMessage:(NSString *) message;
++ (UIAlertView *) alertWithTitle:(NSString *) title message:(NSString *) message;
+
 - (id) initWithMessage:(NSString *) message;
 - (id) initWithTitle:(NSString *) title message:(NSString *) message;
 
--- UIAlertView+Additions.m
+++ UIAlertView+Additions.m
@@ -3,6 +3,14 @@
 
 @implementation UIAlertView (Additions)
 
++ (UIAlertView *) alertWithMessage:(NSString *) message; {
+	return [[UIAlertView alloc] initWithMessage:message];
+}
+
++ (UIAlertView *) alertWithTitle:(NSString *) title message:(NSString *) message; {
+	return [[UIAlertView alloc] initWithTitle:title message:message];
+}
+
 - (id) initWithMessage:(NSString *) message; {
 	self = [super init];
 	self.message = message;
