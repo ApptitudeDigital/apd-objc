3f3a42612a15b3f96549d8c10005adf3691fbade
pixelrevision <pixelrevision@gmail.com>
2015-06-29 15:22:43 -0400

    debug view

---
+++ APDDebugViewController.h
@@ -0,0 +1,27 @@
+#import <UIKit/UIKit.h>
+
+@class APDDebugViewController;
+
+@protocol APDDebugViewControllerDelegate <NSObject>
+- (void)debugViewControllerIsDone:(APDDebugViewController *)debugViewController;
+@end
+
+
+@interface APDDebugViewControllerItem : NSObject
+@property SEL selector;
+@property NSString *name;
+@end
+
+@interface APDDebugViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>{
+	
+}
+
+@property (weak) IBOutlet UITableView *tableView;
+@property (weak) NSObject <APDDebugViewControllerDelegate> *delegate;
+@property NSMutableArray *items;
+
+
++ (APDDebugViewControllerItem *)viewControllerItemWithName:(NSString *)name andSelector:(SEL)selector;
+- (void)insertItemWithName:(NSString *)name andSelector:(SEL)selector;
+
+@end
---
+++ APDDebugViewController.m
@@ -0,0 +1,62 @@
+#import "APDDebugViewController.h"
+
+@implementation APDDebugViewControllerItem
+
+@end
+
+@implementation APDDebugViewController
+
+- (id)init{
+	self = [super init];
+	self.items = [[NSMutableArray alloc] init];
+	return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder{
+	self = [super initWithCoder:aDecoder];
+	self.items = [[NSMutableArray alloc] init];
+	return self;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil{
+	self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+	self.items = [[NSMutableArray alloc] init];
+	return self;
+}
+
+- (void)insertItemWithName:(NSString *)name andSelector:(SEL)selector{
+	APDDebugViewControllerItem *item = [[APDDebugViewControllerItem alloc] init];
+	item.name = name;
+	item.selector = selector;
+	[self.items addObject:item];
+	[self.tableView reloadData];
+}
+
++ (APDDebugViewControllerItem *)viewControllerItemWithName:(NSString *)name andSelector:(SEL)selector{
+	APDDebugViewControllerItem *item = [[APDDebugViewControllerItem alloc] init];
+	item.name = name;
+	item.selector = selector;
+	return item;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+	return self.items.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+	UITableViewCell *cell = [[UITableViewCell alloc] init];
+	APDDebugViewControllerItem *item = [self.items objectAtIndex:indexPath.row];
+	cell.textLabel.text = item.name;
+	return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+	APDDebugViewControllerItem *item = [self.items objectAtIndex:indexPath.row];
+	if([self respondsToSelector:item.selector]){
+		IMP imp = [self methodForSelector:item.selector];
+		void (*func)(id, SEL) = (void *)imp;
+		func(self, item.selector);
+	}
+}
+
+@end
