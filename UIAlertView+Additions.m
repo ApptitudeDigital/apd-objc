15c3307c680ab9f4588bde0d24eefa4c37cddba2
Aaron Smith <gngrwzrd@gmail.com>
2015-12-20 11:19:23 -0800

    UIALertView+Additions

--- UIAlertView+Additions.m
+++ UIAlertView+Additions.m
@@ -12,13 +12,13 @@
 }
 
 - (id) initWithMessage:(NSString *) message; {
-	self = [super init];
+	self = [self init];
 	self.message = message;
 	return self;
 }
 
 - (id) initWithTitle:(NSString *) title message:(NSString *) message; {
-	self = [super init];
+	self = [self init];
 	self.message = message;
 	self.title = title;
 	return self;
